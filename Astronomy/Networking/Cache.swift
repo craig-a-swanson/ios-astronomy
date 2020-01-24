e9af1b1e834910aef881614a2e947c4d479db00a
Craig Swanson <cswanson02@me.com>
2020-01-24 10:24:51 -1000

    Completed making Cache thread-safe

--- Astronomy/Networking/Cache.swift
+++ Astronomy/Networking/Cache.swift
@@ -11,13 +11,19 @@ import Foundation
 class Cache<Key: Hashable, Value> {
     
     private var cacheDictionary: [Key: Value] = [:]
+    let backgroundQueue = DispatchQueue.global(qos: .default)
     
     func cache(value: Value, key: Key) {
-        cacheDictionary.updateValue(value, forKey: key)
+        backgroundQueue.sync {
+            self.cacheDictionary.updateValue(value, forKey: key)
+        }
     }
     
     func value(for key: Key) -> Value? {
-        return cacheDictionary[key] ?? nil
+        return backgroundQueue.sync { () -> Value? in
+            return cacheDictionary[key] ?? nil
+        }
+//        return cacheDictionary[key] ?? nil
     }
 }
 
